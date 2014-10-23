636d70ba48f928870b11c4e61e6a2f284504a3c2
Peter Sudmant <psudmant@mit.edu>
2014-10-23 18:49:27 -0400

    renamed

---
+++ seqlib/count_ss/do_count_ss.sh
@@ -0,0 +1,1 @@
+python count_ss.py --fn_fasta /net/uorf/data/psudmant/genomes/ensembl/release-77/index/homo_sapiens/genome/GRCh38.fa --fn_gff /net/uorf/data/psudmant/genomes/ensembl/release-77/gtf/homo_sapiens/GRCh38.gff
