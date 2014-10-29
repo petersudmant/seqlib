2b42445c9a8c945bfc2e1568d2cb3d13943006ac
Peter Sudmant <psudmant@mit.edu>
2014-10-28 23:39:39 -0400

    sra xml parsing

--- seqlib/sra/generate_download_config.py
+++ seqlib/sra/generate_download_config.py
@@ -2,6 +2,7 @@ import argparse
 import xmltodict
 import os
 import json
+import pdb
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -15,7 +16,7 @@ if __name__=="__main__":
     STUDY_ACC=cwd.split("/")[-1].split("_")[0] 
 
     j_out = {"study_accession":STUDY_ACC}
-    j_out = {"symlink_pattern":["accession","disease state","tissue"]}
+    j_out["symlink_pattern"] = ["accession","disease state","tissue"]
     j_out["samples"] = {}
 
     for d in xml_dict["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]:
@@ -24,14 +25,24 @@ if __name__=="__main__":
         """
         STUDY_accession = d["STUDY"]["@accession"]
         assert STUDY_accession==STUDY_ACC
-
+        
         sample_accession = d["SAMPLE"]["@accession"]
         sample_alias = d["SAMPLE"]["@alias"]
         sample_title = d["SAMPLE"]["TITLE"]
+        print(type(d["RUN_SET"]["RUN"])), len(d["RUN_SET"]["RUN"])
+        #iterate over the 
+        run_accessions = []
+        if type(d["RUN_SET"]["RUN"]) == list:
+            for st in d["RUN_SET"]["RUN"]:
+                run_accessions.append(st["@accession"])
+        else:
+            run_accessions = [d["RUN_SET"]["RUN"]["@accession"]]
 
-        j_out["samples"][sample_accession] = {"accession":sample_accession,
-                                              "alias":sample_alias,
-                                              "title":sample_title}
+        j_out["samples"][sample_accession] = {"sample_accession":sample_accession,
+                                              "sample_alias":sample_alias,
+                                              "sample_title":sample_title,
+                                              "run_accessions":run_accessions
+                                              }
         for attribute in d["SAMPLE"]["SAMPLE_ATTRIBUTES"]["SAMPLE_ATTRIBUTE"]:
             attr = attribute["TAG"]
             val = attribute["VALUE"]
