f1ea40ba89e4cc2c1afb91a62aa6e42b0dc7e227
Peter Sudmant <psudmant@mit.edu>
2014-11-04 11:11:08 -0500

    dunno :)

--- seqlib/sra/generate_download_config.py
+++ seqlib/sra/generate_download_config.py
@@ -4,19 +4,19 @@ import os
 import json
 import pdb
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--fn_xml",default="SraExperimentPackage.xml")
-    o = parser.parse_args()
 
-    xml = open(o.fn_xml).read()
-    xml_dict = xmltodict.parse(xml)
+def make_json_config(xml_dict, naming_pattern, fn_add_to_config):
+    
+    add = {}
+    if fn_add_to_config:
+        add = json.load(open(fn_add_to_config))
 
+    
     cwd = os.getcwd()
     STUDY_ACC=cwd.split("/")[-1].split("_")[0] 
 
     j_out = {"study_accession":STUDY_ACC}
-    j_out["symlink_pattern"] = ["accession","disease state","tissue"]
+    j_out["naming_pattern"] = naming_pattern
     j_out["samples"] = {}
 
     for d in xml_dict["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]:
@@ -29,7 +29,6 @@ if __name__=="__main__":
         sample_accession = d["SAMPLE"]["@accession"]
         sample_alias = d["SAMPLE"]["@alias"]
         sample_title = d["SAMPLE"]["TITLE"]
-        print(type(d["RUN_SET"]["RUN"])), len(d["RUN_SET"]["RUN"])
         #iterate over the 
         run_accessions = []
         if type(d["RUN_SET"]["RUN"]) == list:
@@ -43,15 +42,75 @@ if __name__=="__main__":
                                               "sample_title":sample_title,
                                               "run_accessions":run_accessions
                                               }
-        for attribute in d["SAMPLE"]["SAMPLE_ATTRIBUTES"]["SAMPLE_ATTRIBUTE"]:
-            attr = attribute["TAG"]
-            val = attribute["VALUE"]
-            j_out["samples"][sample_accession][attr] = val
-    
+        if "SAMPLE_ATTRIBUTES" in d["SAMPLE"]:
+            for attribute in d["SAMPLE"]["SAMPLE_ATTRIBUTES"]["SAMPLE_ATTRIBUTE"]:
+                attr = attribute["TAG"]
+                val = attribute["VALUE"]
+                j_out["samples"][sample_accession][attr] = val
+
+        if "samples" in add.keys():
+            for key, value in add["samples"][sample_accession].iteritems():
+                j_out["samples"][sample_accession][key] = value
+
     FOUT = open("%s/config.json"%cwd,'w')
     FOUT.write(json.dumps(j_out, indent=4, separators=(",", ": ")))
     FOUT.close()
 
-    
+   
+
+def manual_parse(xml_dict, manually_currate = []):
+
+    cwd = os.getcwd()
+    STUDY_ACC=cwd.split("/")[-1].split("_")[0] 
+
+    j_out = {"study_accession":STUDY_ACC}
+    j_out["naming_pattern"] = ["sample_accession", "tissue"]
+    j_out["samples"] = {}
+
+    for d in xml_dict["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]:
+        """
+        [u'EXPERIMENT', u'SUBMISSION', u'STUDY', u'SAMPLE', u'RUN_SET']
+        """
+        STUDY_accession = d["STUDY"]["@accession"]
+        assert STUDY_accession==STUDY_ACC
+        
+        sample_accession = d["SAMPLE"]["@accession"]
+        sample_alias = d["SAMPLE"]["@alias"]
+        sample_title = d["SAMPLE"]["TITLE"]
+        
+        j_out["samples"][sample_accession] = {"sample_accession":sample_accession}
+
+        if "SAMPLE_ATTRIBUTES" in manually_currate:
+            #print "OK"
+            #print d["SAMPLE"]["DESCRIPTION"]
+            #attr = raw_input("attribute: ")
+            #val = raw_input("value: ")
+            #j_out["samples"][sample_accession][attr] = val
+            j_out["samples"][sample_accession]["description"] = d["SAMPLE"]["DESCRIPTION"]
+             
+        else:
+            for attribute in d["SAMPLE"]["SAMPLE_ATTRIBUTES"]["SAMPLE_ATTRIBUTE"]:
+                attr = attribute["TAG"]
+                val = attribute["VALUE"]
+                j_out["samples"][sample_accession][attr] = val
+    F = open("manual_add.json",'w') 
+    F.write(json.dumps(j_out, indent=4, separators=(",", ": ")))
+    F.close()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fn_xml",default="SraExperimentPackage.xml")
+    parser.add_argument("--manual_curration", default=None)
+    parser.add_argument("--add_to_config", default=None)
+    parser.add_argument("--naming_pattern", default="sample_accession:tissue")
+    o = parser.parse_args()
+
+    xml = open(o.fn_xml).read()
+    xml_dict = xmltodict.parse(xml)
+
+    if o.manual_curration:
+        manual_parse(xml_dict, o.manual_curration.split(":"))
+    else:
+        make_json_config(xml_dict, o.naming_pattern.split(":"), o.add_to_config)
     
 
