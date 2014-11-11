bc985859cca8490a8b43a2c23f76b54a00ac2204
Peter Sudmant <psudmant@mit.edu>
2014-11-11 15:28:50 -0500

    id up and down NAGNAGs

--- seqlib/splicelib/splicegraph.py
+++ seqlib/splicelib/splicegraph.py
@@ -59,14 +59,14 @@ class SpliceGraph(object):
                 5'-1
             -----  5'-2
             --------
-            this will return 5'-2
+            this will return 5'-1
             BUT, given
             
                 5'-1
             -----  5'-2
             --------
               -- 5'-3
-            this will still return 5'-2, I THINK this is correct...
+            this will still return 5'-1, I THINK this is correct...
         """
         assert ss_type == "5'" or ss_type == "3'"
         assert strand == "FWD" or strand == "REV"
@@ -95,62 +95,72 @@ class SpliceGraph(object):
         """
         get all NAGNAGs on FWD strand
         """
-        for ss_5p, ss_3p_list in self.F_5p_3p_ss.iteritems():
-            annotated_ss_3p = np.sort(np.array(ss_3p_list))
-            max_ss_3p = annotated_ss_3p[-1]
-            ds_exon = self.get_common_shortest_exon(max_ss_3p, "3'", "FWD")
-            us_exon = self.get_common_shortest_exon(ss_5p, "5'", "FWD")
-            us_gene_inf = self.F_5p_to_gene_info[us_exon[1]]
-            ds_gene_inf = self.F_3p_to_gene_info[ds_exon[0]]
-            
-            gene_inf = []
-            for inf in us_gene_inf+ds_gene_inf:
-                if not inf in gene_inf: gene_inf.append(inf)
-            
-            k = min(n, max_ss_3p-ss_5p)
-            alt_3p_AG = np.array([m.start()+(max_ss_3p-k)+2 for m in re.finditer("AG", seq[max_ss_3p-k:max_ss_3p-2])])
-            
-            for ss_3p in alt_3p_AG:
-                if ss_3p in annotated_ss_3p: 
-                    source = "NAGNNAG_annotated"
-                else:
-                    source = "NAGNNAG"
-                #currently gets EXTANT NAGNAGS
-                 
-                exon_paths = {"A":[0,1], "B":[0,2]}
-                EXONS  = [us_exon, [ss_3p, ds_exon[1]], ds_exon]
-                FEATURE_ID = ",".join(["%s:%d-%d"%(self.contig, e[0], e[1]) for e in EXONS])
-                GENE_NAME = ",".join(["%s"%gi['gene_name'] for gi in gene_inf])
-                GENE_ID = ",".join(["%s"%gi['gene_ID'] for gi in gene_inf])
-                G_START = us_exon[0]
-                G_END = ds_exon[1]
-                STRAND = 1
 
-                nagNnag_T = Transcript(contig = self.contig, 
-                                       feature_ID = FEATURE_ID,
-                                       exons = EXONS, 
-                                       gene_name = GENE_NAME,
-                                       gene_ID = GENE_ID,
-                                       g_start = G_START,
-                                       g_end = G_END,
-                                       strand = STRAND)
-                 
-                gff_s = nagNnag_T.gff_string(exon_paths, source)
-                bed_s = nagNnag_T.bed_string(exon_paths, source, True)
-                F_gff.write(gff_s)
-                F_bed.write(bed_s)
+        for strand_d, _5p_3p_ss in { "FWD" : self.F_5p_3p_ss, "REV" : self.R_5p_3p_ss }.iteritems():
+            
+            for ss_5p, ss_3p_list in _5p_3p_ss.iteritems():
+                annotated_ss_3p = np.sort(np.array(ss_3p_list))
+
+                max_ss_3p = annotated_ss_3p[-1]
+                min_ss_3p = annotated_ss_3p[0]
                 
-        #REV
-        """
-        get all NAGNAGs on REV strand
-        """
-        #for ss_5p, ss_3p_list in self.R_5p_3p_ss.iteritems():
-        #    ss_3ps = np.sort(np.array(ss_3p_list))
-        #    d = np.diff(ss_3ps)
-        #    for w in np.where(d<=n)[0]:
-        #        print "R:", self.contig, ss_3ps[w], ss_3ps[w+1], ss_5p, d[w]
-            
+                if strand_d == "FWD":
+                    annot_3p_ss = max_ss_3p
+                else:
+                    annot_3p_ss = min_ss_3p
 
+                ds_exon = self.get_common_shortest_exon(annot_3p_ss, "3'", strand_d)
+                us_exon = self.get_common_shortest_exon(ss_5p, "5'", strand_d)
+
+                us_gene_inf = self.F_5p_to_gene_info[ss_5p]
+                ds_gene_inf = self.F_3p_to_gene_info[annot_3p_ss]
+
+                gene_inf = []
+                for inf in us_gene_inf+ds_gene_inf:
+                    if not inf in gene_inf: gene_inf.append(inf)
+            
+                """
+                k is the number of bp up/down of the annotated 3p ss to search for now ones
+                """
+                
+                k = min(n, abs(annot_ss_3p-ss_5p))
+                ss_seq = strand_d == "FWD" and "AG" or "CT"
+                if strand_d == "FWD":
+                    seq_s, seq_e = annot_3p_ss-k, annot_3p_ss-2
+                else:
+                    seq_s, seq_e = annot_3p_ss+2, annot_3p_ss+k
+                
+                alt_3p_ss = np.array([m.start()+seq_s for m in re.finditer(seq_s, seq_e)])
+            
+                for ss_3p in alt_3p_ss:
+                    if ss_3p in annotated_ss_3p: 
+                        source = "NAGNNAG_annotated"
+                    else:
+                        source = "NAGNNAG"
+                     
+                    exon_paths = {"A":[0,1], "B":[0,2]}
+                    NAG_exon = strand_d == "FWD" and [ss_3p, ds_exon[1]] or [ds_exon[0], ss_3p]
+                    EXONS  = [us_exon, NAG_exon, ds_exon]
+                    FEATURE_ID = ",".join(["%s:%d-%d"%(self.contig, e[0], e[1]) for e in EXONS])
+                    GENE_NAME = ",".join(["%s"%gi['gene_name'] for gi in gene_inf])
+                    GENE_ID = ",".join(["%s"%gi['gene_ID'] for gi in gene_inf])
+                    G_START = min(us_exon[0], ds_exon[0])
+                    G_END = max(us_exon[1], ds_exon[1])
+                    STRAND = strand_d == "FWD" and 1 or -1
+
+                    nagNnag_T = Transcript(contig = self.contig, 
+                                           feature_ID = FEATURE_ID,
+                                           exons = EXONS, 
+                                           gene_name = GENE_NAME,
+                                           gene_ID = GENE_ID,
+                                           g_start = G_START,
+                                           g_end = G_END,
+                                           strand = STRAND)
+                     
+                    gff_s = nagNnag_T.gff_string(exon_paths, source)
+                    bed_s = nagNnag_T.bed_string(exon_paths, source, True)
+                    F_gff.write(gff_s)
+                    F_bed.write(bed_s)
 
     def enumerate_splice_junctions(self, seq):
         fwd_5p = defaultdict(int)
@@ -178,15 +188,18 @@ class SpliceGraph(object):
         print rev_5p, np.sum(rev_5p.values())
         print rev_3p, np.sum(rev_3p.values())
 
-def init_splice_graphs_from_gff(fn_gff, contigs = [], features = ["transcript", "mRNA", "protein_coding"]):
+
+def init_splice_graphs_from_gff3(fn_gff, **kwargs):
     """
     limit_info={"gff_id": ["13"]}
     """
+    contigs = kwargs.get('contigs', [])
+    features = kwargs.get('features', ["transcript", "mRNA", "protein_coding"])
+
     limit_info = {"gff_id": contigs}
     GFF_parser = GFF.GFFParser()
     
     SGs_by_contig = {}
-    
     for rec in GFF_parser.parse_in_parts(open(fn_gff), limit_info=limit_info):
         #iterate over chromosomes
         contig = rec.id
