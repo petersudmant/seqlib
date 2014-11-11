0e0eefd4420b0a6edca77fcd1310fc43701a436b
Peter Sudmant <psudmant@mit.edu>
2014-11-11 15:28:35 -0500

    ucsc chrs

--- seqlib/splicelib/id_nagnag.py
+++ seqlib/splicelib/id_nagnag.py
@@ -17,18 +17,21 @@ if __name__=="__main__":
     parser.add_argument("--fn_input_gff")
     parser.add_argument("--fn_output_gff")
     parser.add_argument("--fn_output_bed")
+    parser.add_argument("--track_desc")
     o = parser.parse_args()
     
     fa = FastaHack(o.fn_fasta)
     
     contigs = [contig for contig in fa.names]
-    contigs = ["19"]
+    contigs = ["chr20"]
     #contigs = ["GL000213.1"]
-    splice_graphs_by_contig = sg.init_splice_graphs_from_gff(o.fn_input_gff, contigs)
+
+    
+    splice_graphs_by_contig = sg.init_splice_graphs_from_gff3(o.fn_input_gff, contigs=contigs)
         
     F_gff = open(o.fn_output_gff,'w')
     F_bed = open(o.fn_output_bed,'w')
-    F_bed.write("""track name=HSA_NAGNNAGs description="HSA NAGNNAGS" visibility=2\n""")
+    F_bed.write("""track name=NAGNNAG description="%s NAGNNAGs" visibility=2\n"""%(o.track_desc))
     
     for contig, splice_graph in splice_graphs_by_contig.iteritems():
         seq = fa.get_sequence(contig)
