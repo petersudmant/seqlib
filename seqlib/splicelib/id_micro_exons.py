b0526b13645fa375c63817a9b54360be13630a85
Peter Sudmant <psudmant@mit.edu>
2015-01-16 15:07:27 -0500

    micro exon getter

--- seqlib/splicelib/id_micro_exons.py
+++ seqlib/splicelib/id_micro_exons.py
@@ -16,11 +16,12 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--fn_fasta")
-    parser.add_argument("--fn_input_gff_dir")
+    parser.add_argument("--fn_input_gff")
     parser.add_argument("--fn_output_gff_dir", default="gff")
     parser.add_argument("--fn_output_bed_dir", default="bed")
-    parser.add_argument("--fn_output_juncs_dir", default="juncs")
+    parser.add_argument("--fn_output_juncs_dir", default="junctions")
     parser.add_argument("--track_desc")
+    parser.add_argument("--micro_exon_type", choices=['skipped','5p','3p'], required=True)
     parser.add_argument("--max_alt_exon_len", default=60, type=int)
     o = parser.parse_args()
     
@@ -32,36 +33,34 @@ if __name__=="__main__":
     
     splice_graphs_by_contig = sg.init_splice_graphs_from_gff3(o.fn_input_gff, contigs=contigs)
     
-    micro_exon_types = ["5p_micro_exon", "3p_micro_exon"]
-    
-    for mx_type in micro_exon_types:
-        F_gff = open("{gff_dir}/{mx_type}.gff".format(gff_dir=o.fn_output_gff_dir,
-                                                      mx_type=mx_type),'w')
-        F_gff = open("{gff_dir}/{mx_type}.novel.gff".format(gff_dir=o.fn_output_gff_dir,
-                                                            mx_type=mx_type),'w')
-        F_bed = open("{bed_dir}/{mx_type}.novel.gff".format(bed_dir=o.fn_output_bed_dir,
-                                                            mx_type=mx_type),'w')
-        track_desc="""track name=NAGNNAG description={track_desc} {mx_type}" visibility=2\n""".format(track_desc=o.track_desc,
-                                                                                                      mx_type==mx_type)
-        F_bed.write(track_desc)
+    mx_type = o.micro_exon_type 
+    F_gff = open("{gff_dir}/{mx_type}.gff".format(gff_dir=o.fn_output_gff_dir,
+                                                  mx_type=mx_type),'w')
+    F_novel_gff = open("{gff_dir}/{mx_type}.novel.gff".format(gff_dir=o.fn_output_gff_dir,
+                                                              mx_type=mx_type),'w')
+    F_bed = open("{bed_dir}/{mx_type}.bed".format(bed_dir=o.fn_output_bed_dir,
+                                                        mx_type=mx_type),'w')
+    track_desc="""track name="{mx_type}" description="{track_desc} {mx_type}" visibility=2 itemRgb=On\n""".format(track_desc=o.track_desc,
+                                                                                                  mx_type=mx_type)
+    F_bed.write(track_desc)
 
-        j_writer = JunctionWriter("{junction_dir}/{mex_type}".format(junction_dir=o.fn_output_juncs_dir,
-                                                                     mx_type=mx_type))
+    j_writer = JunctionWriter("{junction_dir}/{mx_type}".format(junction_dir=o.fn_output_juncs_dir,
+                                                                 mx_type=mx_type))
 
-        for contig, splice_graph in splice_graphs_by_contig.iteritems():
-            seq = fa.get_sequence(contig)
-            #splice_graph.enumerate_splice_junctions(seq)
-            if mx_type=="5p_micro_exon" or mx_type=="3p_micro_exon":
-                get_3p=mx_type=="3p_micro_exon"
-                get_5p=mx_type=="5p_micro_exon"
-                splice_graph.get_5p_3p_alt_exon(seq, 
-                                                F_gff, 
-                                                F_novel_gff, 
-                                                F_bed, 
-                                                j_writer, 
-                                                get_5p=get_5p,
-                                                get_3p=get_3p,
-                                                n=o.max_alt_exon_len)
-            else:
-                print "NOT IMPLEMENTED"
+    for contig, splice_graph in splice_graphs_by_contig.iteritems():
+        seq = fa.get_sequence(contig)
+        #splice_graph.enumerate_splice_junctions(seq)
+        if mx_type=="5p" or mx_type=="3p":
+            get_3p=mx_type=="3p"
+            get_5p=mx_type=="5p"
+            splice_graph.get_5p_3p_alt_exon(seq, 
+                                            F_gff, 
+                                            F_novel_gff, 
+                                            F_bed, 
+                                            j_writer, 
+                                            get_5p=get_5p,
+                                            get_3p=get_3p,
+                                            n=o.max_alt_exon_len)
+        else:
+            print "NOT IMPLEMENTED"
 
