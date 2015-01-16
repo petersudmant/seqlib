21b5c4fba829a342251f9715633d99c2503aee19
Peter Sudmant <psudmant@mit.edu>
2015-01-15 19:20:51 -0500

    annotate any and all 3' or 5' microexons

--- seqlib/splicelib/splicegraph.py
+++ seqlib/splicelib/splicegraph.py
@@ -49,7 +49,7 @@ class SpliceGraph(object):
         self.R_5p_to_gene_info = kwargs["R_5p_to_gene_info"]
         self.R_3p_to_gene_info = kwargs["R_3p_to_gene_info"]
         
-    def get_common_shortest_exon(self, ss, ss_type, strand):
+    def get_common_shortest_exon(self, ss, strand, ss_type_3p=False, ss_type_5p=False):
         """
         ss_type = 5' | 3'
         strand = FWD | REV 
@@ -69,16 +69,16 @@ class SpliceGraph(object):
               -- 5'-3
             this will still return 5'-1, I THINK this is correct...
         """
-        assert ss_type == "5'" or ss_type == "3'"
-        assert strand == "FWD" or strand == "REV"
+        assert ss_type_3p!=ss_type_5p, "must pick 3' or 5' (_3p, _5p)"
+        assert strand=="FWD" or strand=="REV", "must pick FWD or REV (FWD, REV)"
 
-        if strand == "FWD":
-            if ss_type == "5'":
+        if strand=="FWD":
+            if ss_type_5p:
                 return tuple([max(self.F_exon_e_s[ss]), ss])
             else:
                 return tuple([ss, min(self.F_exon_s_e[ss])])
         else:
-            if ss_type == "5'":
+            if ss_type_5p:
                 return tuple([ss, min(self.R_exon_s_e[ss])])
             else:
                 return tuple([max(self.R_exon_e_s[ss]), ss])
@@ -109,11 +109,11 @@ class SpliceGraph(object):
 
             for ss_5p, ss_3p_list in _5p_3p_ss.iteritems():
                 
-                us_exon = self.get_common_shortest_exon(ss_5p, "5'", strand_d)
+                us_exon = self.get_common_shortest_exon(ss_5p, strand_d, ss_type_5p=True)
                 us_gene_inf = _5p_to_gene_info[ss_5p]
                 
                 for ss_3p in ss_3p_list:
-                    ds_exon = self.get_common_shortest_exon(ss_3p, "3'", strand_d)
+                    ds_exon = self.get_common_shortest_exon(ss_3p,  strand_d, ss_type_3p=True)
                     ds_gene_inf = _3p_to_gene_info[ss_3p]
                     
                     ss_5p_seq = strand_d == "FWD" and "GT" or "AC"
@@ -128,7 +128,140 @@ class SpliceGraph(object):
                     #OK, now ID the chunks that fit the bill!, ie, ss- < n bases -ss
 
 
-    def get_NAGNAGs(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, n=60):
+
+    def get_5p_micro_exon(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, micro_exon_dir, n=60):
+        get_5p_3p_alt_exon(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, get_5p=True, n=n)
+    
+    def get_3p_micro_exon(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, micro_exon_dir, n=60):
+        get_5p_3p_alt_exon(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, get_3p=True, n=n)
+
+    def get_5p_3p_alt_exon(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, get_5p=False, get_3p=False, n=60):
+        """
+        get all putative alternative 5'/3' alternative exons 
+        """
+        
+        assert get_5p != get_3p, "EITHER get_5p or get_3p (5' or 3') must be passed"
+
+        if get_3p:
+            fwd_ss_juncs = self.F_5p_3p_ss
+            rev_ss_juncs = self.R_5p_3p_ss
+        else:
+            fwd_ss_juncs = self.F_3p_5p_ss
+            rev_ss_juncs = self.R_3p_5p_ss
+
+        for strand_d, ss_juncs in { "FWD" : fwd_ss_juncs, "REV" : rev_ss_juncs }.iteritems():
+            """
+            whether we are looking for alt 5' or 3' ss, one will be held constant while 
+            we scan around the other, call the one which we hold constant to be "fixed"
+            and the other to be called alt
+            """
+            for fixed_ss_3_or_5, alt_annot_ss_5_or_3_list in ss_juncs.iteritems():
+                for alt_annot_5_or_3 in alt_annot_ss_5_or_3_list: 
+                    if get_3p:
+                        curr_3p = alt_annot_5_or_3
+                        curr_5p = fixed_ss_3_or_5
+                    if get_5p:
+                        curr_3p = fixed_ss_3_or_5   
+                        curr_5p = alt_annot_5_or_3
+
+                ds_exon = self.get_common_shortest_exon(curr_3p, strand_d, ss_type_3p=True)
+                us_exon = self.get_common_shortest_exon(curr_5p, strand_d, ss_type_5p=True)
+                
+                """
+                GET GENE INFO
+                """
+                if strand_d == "FWD":
+                    us_gene_inf = self.F_5p_to_gene_info[curr_5p]
+                    ds_gene_inf = self.F_3p_to_gene_info[curr_3p]
+                else:
+                    us_gene_inf = self.R_5p_to_gene_info[curr_5p]
+                    ds_gene_inf = self.R_3p_to_gene_info[curr_3p]
+
+                gene_inf = []
+                for inf in us_gene_inf+ds_gene_inf:
+                    if not inf in gene_inf: gene_inf.append(inf)
+            
+                """
+                k is the number of bp up/down of the annotated 
+                5p/3p ss to search for now ones, ie, don't search past 
+                annotated OTHER 5' splice site!
+                """
+                
+                if get_3p:
+                    ss_seq = strand_d == "FWD" and "AG" or "CT"
+                else:
+                    ss_seq = strand_d == "FWD" and "GT" or "AC"
+                        
+                max_k = min(n, abs(curr_5p-curr_3p))
+            
+                min_alt, max_alt = alt_annot_5_or_3-k, alt_annot_5_or_3+k
+                if strand_d == "FWD":
+                    #seq_s, seq_e = annot_3p_ss-k, annot_3p_ss-2
+                    if get_5p:
+                        seq_s, seq_e = max(min_alt, us_exon[0]+1), min(max_alt, ds_exon[0])
+                    else: 
+                        seq_s, seq_e = max(min_alt, us_exon[1]+1), min(max_alt, ds_exon[1])
+                else:
+                    if get_5p:
+                        seq_s, seq_e = max(min_alt, ds_exon[1]+1), min(max_alt, us_exon[1])
+                    else:
+                        seq_s, seq_e = max(min_alt, ss_exon[0]+1), min(max_alt, us_exon[0])
+                """
+                offset by a 2 if in fwd dir as you want exon 
+                EXON start, not the, position of the ss
+                for rev, the position is fine already
+                """
+                delta = strand_d == "FWD" and 2 or 0 
+                alt_ss_list = np.array([m.start()+seq_s+delta for m in re.finditer(ss_seq, seq[seq_s:seq_e].upper())])
+                #############
+                for alt_ss in alt_ss_list:
+                    if alt_ss == alt_annot_5_or_3: 
+                        continue
+
+                    if alt_ss in alt_annot_ss_5_or_3_list: 
+                        source = "annotated"
+                    else:
+                        source = "novel"
+                    
+                    exon_paths = {"A":[0,1], "B":[0,2]}
+                    if "FWD" and get_3p:
+                        alt_exon=[alt_ss, ds_exon[1]]
+                    elif "REV" and get_3p:
+                        alt_exon=[ds_exon[0], alt_ss]
+                    elif "FWD" and get_5p:
+                        alt_exon=[us_exon[0], alt_ss]
+                    elif "REV" and get_5p:
+                        alt_exon=[alt_ss, us_exon[1]]
+
+                    EXONS  = [us_exon, alt_exon, ds_exon]
+                    FEATURE_ID = "_".join(["%s:%d-%d"%(self.contig, e[0], e[1]) for e in EXONS])
+                    GENE_NAME = ",".join(["%s"%gi['gene_name'] for gi in gene_inf])
+                    GENE_ID = "_".join(["%s"%gi['gene_ID'] for gi in gene_inf])
+                    G_START = min(us_exon[0], ds_exon[0])
+                    G_END = max(us_exon[1], ds_exon[1])
+                    STRAND = strand_d == "FWD" and 1 or -1
+                    
+                    alt_T = Transcript(contig = self.contig, 
+                                       feature_ID = FEATURE_ID,
+                                       exons = EXONS, 
+                                       gene_name = GENE_NAME,
+                                       gene_ID = GENE_ID,
+                                       g_start = G_START,
+                                       g_end = G_END,
+                                       strand = STRAND)
+
+                    gff_s = alt_T.gff_string(exon_paths, source)
+                    gff_novel_s = alt_T.gff_string({"A":[0,1]}, source)
+
+                    bed_s = alt_T.bed_string(exon_paths, source, True)
+                    junc_tups = alt_T.junc_tuples(exon_paths, source, True)
+                    F_gff.write(gff_s)
+                    F_novel_gff.write(gff_novel_s)
+                    F_bed.write(bed_s)
+                    junc_writer.write(junc_tups)
+
+
+    def __DEPRECATED__get_NAGNAGs(self, seq, F_gff, F_novel_gff, F_bed, junc_writer, n=60):
         """
         define NAGNAG
         SD SA1,SA2 where SA1 and SA2 are within n bp of each other
@@ -146,8 +279,8 @@ class SpliceGraph(object):
                 else:
                     annot_3p_ss = min_ss_3p
 
-                ds_exon = self.get_common_shortest_exon(annot_3p_ss, "3'", strand_d)
-                us_exon = self.get_common_shortest_exon(ss_5p, "5'", strand_d)
+                ds_exon = self.get_common_shortest_exon(annot_3p_ss, strand_d, ss_type_3p=True)
+                us_exon = self.get_common_shortest_exon(ss_5p,  strand_d, ss_type_5p=True)
                 
                 """
                 GET GENE INFO
