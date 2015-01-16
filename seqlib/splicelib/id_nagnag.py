2a748d47e0a2fe0b085e0a7db799d5fd4760f187
Peter Sudmant <psudmant@mit.edu>
2015-01-16 14:56:26 -0500

    get NAGNAGs using generic 3' utitilites

--- seqlib/splicelib/id_nagnag.py
+++ seqlib/splicelib/id_nagnag.py
@@ -17,32 +17,44 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fn_fasta")
     parser.add_argument("--fn_input_gff")
-    parser.add_argument("--fn_output_gff")
-    parser.add_argument("--fn_output_novel_junc_gff")
-    parser.add_argument("--fn_output_bed")
-    parser.add_argument("--fn_output_juncs_prefix")
+    parser.add_argument("--fn_output_gff_dir", default="gff")
+    parser.add_argument("--fn_output_bed_dir", default="bed")
+    parser.add_argument("--fn_output_juncs_dir", default="junctions")
     parser.add_argument("--track_desc")
-    parser.add_argument("--max_alt_exon_len", default=60)
+    parser.add_argument("--max_alt_exon_len", default=3, type=int)
     o = parser.parse_args()
     
     fa = FastaHack(o.fn_fasta)
     
     contigs = [contig for contig in fa.names]
-    #contigs = ["chr1"]
-    print contigs
     
     splice_graphs_by_contig = sg.init_splice_graphs_from_gff3(o.fn_input_gff, contigs=contigs)
-        
-    F_gff = open(o.fn_output_gff,'w')
-    F_novel_gff = open(o.fn_output_novel_junc_gff,'w')
-    F_bed = open(o.fn_output_bed,'w')
-    j_writer = JunctionWriter(o.fn_output_juncs_prefix)
+    mx_type = "NAGNAG"
 
-    F_bed.write("""track name=NAGNNAG description="%s NAGNNAGs" visibility=2\n"""%(o.track_desc))
-    
+    F_gff = open("{gff_dir}/{mx_type}.gff".format(gff_dir=o.fn_output_gff_dir,
+                                                  mx_type=mx_type),'w')
+    F_novel_gff = open("{gff_dir}/{mx_type}.novel.gff".format(gff_dir=o.fn_output_gff_dir,
+                                                              mx_type=mx_type),'w')
+    F_bed = open("{bed_dir}/{mx_type}.bed".format(bed_dir=o.fn_output_bed_dir,
+                                                        mx_type=mx_type),'w')
+    track_desc="""track name="{mx_type}" description="{track_desc} {mx_type}" visibility=2 itemRgb=On\n""".format(track_desc=o.track_desc,
+                                                                                                  mx_type=mx_type)
+    F_bed.write(track_desc)
+
+    j_writer = JunctionWriter("{junction_dir}/{mx_type}".format(junction_dir=o.fn_output_juncs_dir,
+                                                                 mx_type=mx_type))
+
     for contig, splice_graph in splice_graphs_by_contig.iteritems():
         seq = fa.get_sequence(contig)
-        splice_graph.enumerate_splice_junctions(seq)
-        splice_graph.get_NAGNAGs(seq, F_gff, F_novel_gff, F_bed, j_writer, n=o.max_alt_exon_len)
-    
+        #splice_graph.enumerate_splice_junctions(seq)
+        get_3p=True
+        get_5p=False
+        splice_graph.get_5p_3p_alt_exon(seq, 
+                                        F_gff, 
+                                        F_novel_gff, 
+                                        F_bed, 
+                                        j_writer, 
+                                        get_5p=get_5p,
+                                        get_3p=get_3p,
+                                        n=o.max_alt_exon_len)
 
