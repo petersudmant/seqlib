8a7602c839dd3eb86d814960790c82c249b74b59
psudmant <psudmant@login001.cm.cluster>
2015-04-06 21:28:12 -0400

    plot expression

---
+++ seqlib/expression/do_plot_expression.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+#t=read.table("/Users/psudmant/Documents/science/mounts/c3ddb01/home/psudmant/projects/ancient_exons/timecourse_expression/RbFox_levels/output/RbFOX_expression.df",header=T,sep="\t")
+
+args <- commandArgs(trailingOnly=TRUE)
+input.file <- args[1]
+output.file <- args[2]
+
+
+t=read.table(input.file,header=T,sep="\t")
+
+xlabels=unique(t$alt_name[order(t$group)])
+xbreaks=seq(1,length(xlabels))
+
+pdf(output.file,width=2.5,height=3.5)
+g=ggplot(t)
+g+geom_line(aes(x=group,y=fpkm,color=GeneName))+geom_point(aes(x=group,y=fpkm,group=group,color=GeneName),size=3)+theme_bw()+scale_color_brewer(palette="Set1")+theme_bw()+scale_color_brewer(palette="Set1")+theme(legend.title=element_blank(),legend.key=element_blank(),legend.position="bottom",legend.direction='horizontal',axis.text.x=element_text(angle=45,hjust=1,vjust=1))+guides(color=guide_legend(nrow=2))+scale_x_continuous("",breaks=xbreaks,labels=xlabels)+scale_y_continuous("FPKM")
+
+dev.off()
