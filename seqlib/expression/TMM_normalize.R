8e8c512423be97ecf6b46ec1de4afb3f7b35d635
psudmant <psudmant@login001.cm.cluster>
2015-07-08 10:16:23 -0400

    TMM normalize set of expression counts using edgeR

---
+++ seqlib/expression/TMM_normalize.R
@@ -0,0 +1,16 @@
+library(R.utils)
+
+args = cmdArgs()
+fn_input = cmdArg("fn_input")
+fn_output = cmdArg("fn_output")
+counts_start_col = cmdArg("exp_start_col",default=6)
+
+counts_t = read.table(fn_input,header=T,sep="\t")
+counts_end_col = dim(counts_t)[2]
+
+y=DGEList(counts=counts_t[,counts_start_col:exp_end_col])
+rownames(y)=counts_t$gene_name
+y=calcNormFactors(y)
+adj=t(t(y$counts)/(y$samples$norm.factors * y$samples$lib.size))
+
+write.table(adj, file=fn_output, sep="\t")
