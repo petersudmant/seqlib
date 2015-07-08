d8814731e0a6125aa5f30372da5c715140db649a
psudmant <psudmant@login001.cm.cluster>
2015-07-08 11:54:06 -0400

    fix cols and output

--- seqlib/expression/TMM_normalize.R
+++ seqlib/expression/TMM_normalize.R
@@ -1,4 +1,5 @@
 library(R.utils)
+library(edgeR)
 
 args = cmdArgs()
 fn_input = cmdArg("fn_input")
@@ -8,9 +9,9 @@ counts_start_col = cmdArg("exp_start_col",default=6)
 counts_t = read.table(fn_input,header=T,sep="\t")
 counts_end_col = dim(counts_t)[2]
 
-y=DGEList(counts=counts_t[,counts_start_col:exp_end_col])
+y=DGEList(counts=counts_t[,counts_start_col:counts_end_col])
 rownames(y)=counts_t$gene_name
 y=calcNormFactors(y)
 adj=t(t(y$counts)/(y$samples$norm.factors * y$samples$lib.size))
 
-write.table(adj, file=fn_output, sep="\t")
+write.table(adj, file=fn_output, sep="\t", col.names=NA)
