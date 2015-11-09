d6f3b4da5c99fdf57a2828bd2f8f5fd9cce140ee
Peter Sudmant <psudmant@gmail.com>
2015-11-09 14:51:33 -0500

    use bx tools to examine intervals

---
+++ seqlib/utils/intervaltools.py
@@ -0,0 +1,51 @@
+from bx.intervals.intersection import Interval, IntervalTree
+from bx.intervals.intersection import ClusterTree
+
+class ClusterTool(object):
+    """
+    get overlapping / non-overlapping clusters
+    """
+    def __init__(self):
+        self.idx = 0
+        self.objects_by_idx = {}
+        self.cluster_trees_by_contig = {}
+
+    def get_next_idx(self):
+        self.idx+=1
+        return self.idx
+    
+    def insert(self, contig, s, e, item=None):
+        idx = self.get_next_idx()
+        self.objects_by_idx[idx] = item
+        if not contig in self.cluster_trees_by_contig:
+            self.cluster_trees_by_contig[contig] = ClusterTree(0,0) 
+        
+        self.cluster_trees_by_contig[contig].insert(s,e,idx)
+                    
+    def getregions(self):
+        ret = {}
+        for contig, tree in self.cluster_trees_by_contig.items():
+            ret_list = []
+            for cluster_tup in tree.getregions():
+                s,e, idxs = cluster_tup
+                ret_list.append((s,e,[self.objects_by_idx[idx] for idx in idxs]))
+
+            ret[contig] = ret_list
+        
+        return ret
+
+class IntervalTool(object):
+    """
+    check if something overlaps a region quickly
+    """
+
+    def __init__(self):
+        self.intervals_by_contig = {}
+
+    def insert(self, contig, s, e, item=None):
+
+        if not contig in self.intervals_by_contig:
+            self.intervals_by_contig[contig] = IntervalTree()
+        
+        self.intervals_by_contig[contig].insert_interval(Interval(s,e, item))
+    
