78fdfe0687c709de8d07175d3a4dcb77d63837c8
Peter Sudmant <psudmant@gmail.com>
2016-04-08 13:19:40 -0400

    modify defaults

--- seqlib/sequence/kmercompare.py
+++ seqlib/sequence/kmercompare.py
@@ -49,6 +49,7 @@ class count_kmers_process(mp.Process):
             c2 = self.s2.count(kmer)
             #OR, p_f = fisher_exact([[c1,self.l1], [c2,self.l2]])
             chi, p_chi, dof, exp = chi2_contingency([[c1,self.l1], [c2,self.l2]])
+            if c2==0: c2=1 
             OR = (c1/float(self.l1))/(c2/float(self.l2))
             kmer_counts.append({"s1":c1,
                                 "s2":c2,
@@ -65,16 +66,19 @@ class count_kmers_process(mp.Process):
 
 class KmerCompare():
 
-    def __init__(self, kmer_len=5):
+    def __init__(self, kmer_len=6):
         self.kmer_gen = itertools.product("ATCG",repeat=kmer_len)
         self.kmer_len = kmer_len
 
-    def count_kmers(self, seqs1, seqs2, n_procs = 4):
+    def count_kmers(self, seqs1, seqs2, n_procs = 16):
         """
         pass in two lists of stringsa
         return counts
         """
-
+        
+        self.seqs1 = seqs1
+        self.seqs2 = seqs2
+        
         l1 = np.sum([len(s)-self.kmer_len for s in seqs1])
         l2 = np.sum([len(s)-self.kmer_len for s in seqs2])
         
@@ -119,17 +123,50 @@ class KmerCompare():
             self.kmer_counts.extend(_kmer_counts)
             print("done", file=sys.stderr)
 
-    def get_significant_kmers(self, fdr = 0.05):
+    def get_kmer_stats(self, kmer_count):
+        kmer = kmer_count['kmer'] 
+        s1_cs = np.array([seq.count(kmer) for seq in self.seqs1])
+        s2_cs = np.array([seq.count(kmer) for seq in self.seqs2])
+        
+        mu_s1, med_s1, mx_s1 = np.mean(s1_cs), np.median(s1_cs), np.amax(s1_cs)
+        mu_s2, med_s2, mx_s2 = np.mean(s2_cs), np.median(s2_cs), np.amax(s1_cs)
+        n_s1_gt0 = np.sum(s1_cs>0) 
+        n_s2_gt0 = np.sum(s2_cs>0) 
+        
+        return {"mu_s1_count":mu_s1,
+                "med_s1_count":med_s1,
+                "max_s1_count":mx_s1,
+                "n_s1_w_kmer":n_s1_gt0,
+                "mu_s2_count":mu_s2,
+                "med_s2_count":med_s2,
+                "max_s2_count":mx_s2,
+                "n_s2_w_kmer":n_s2_gt0}
+
+
+
+    def get_significant_kmers(self, fdr = 0.01, Yekuteili=True):
         self.kmer_counts  = sorted(self.kmer_counts, key = lambda x: x['p'])
         m = float(len(self.kmer_counts))
+        cm=1
+        if Yekuteili:
+            """
+            correct for non-independence of tests 
+            """
+            cm =  np.sum(1/(np.arange(m)+1))
+
         outrows = [] 
         for i, kmer_count in enumerate(self.kmer_counts):
             p = kmer_count['p']
             j=i+1
-            if p>((j/m)*fdr):
-                break
+            if p<=((j/(m*cm))*fdr):
+                BHY_sig = True
             else:
-                outrows.append(kmer_count)
+                BHY_sig = False
+
+            kmer_stats = self.get_kmer_stats(kmer_count)
+            kmer_count.update(kmer_stats)
+            kmer_count.update({"BHY-significant":BHY_sig})
+            outrows.append(kmer_count)
         
         return pd.DataFrame(outrows)
 
