6aeb0de91145fae732708271fec9517253f6114f
Peter Sudmant <psudmant@gmail.com>
2016-12-28 11:49:41 -0500

    modified to allow headers to be added

--- seqlib/utils/table_to_feather.py
+++ seqlib/utils/table_to_feather.py
@@ -8,7 +8,17 @@ if __name__=="__main__":
     parser.add_argument("--fn_input")
     parser.add_argument("--fn_output")
     parser.add_argument("--compression", default=None)
+    parser.add_argument("--column_names", default=None, n_args="+")
     o = parser.parse_args()
     
-    t = pd.read_csv(o.fn_input, header=0, sep="\t", compression=o.compression)
+    if o.column_names==None:
+        t = pd.read_csv(o.fn_input, header=0, sep="\t", compression=o.compression)
+    else:
+        t = pd.read_csv(o.fn_input, 
+                        header=0, 
+                        sep="\t", 
+                        compression=o.compression, 
+                        header=None,
+                        names=o.column_names)
+    
     feather.write_dataframe(t, o.fn_output)
