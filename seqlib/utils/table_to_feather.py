c825dcb66324daa01f592596ff3d48a3e422ff1e
Peter Sudmant <psudmant@gmail.com>
2017-01-06 10:54:05 -0500

    fix typo in args headerless table reader

--- seqlib/utils/table_to_feather.py
+++ seqlib/utils/table_to_feather.py
@@ -1,21 +1,20 @@
 import argparse
 import pandas as pd
 import feather
-
+import pdb
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()    
     parser.add_argument("--fn_input")
     parser.add_argument("--fn_output")
     parser.add_argument("--compression", default=None)
-    parser.add_argument("--column_names", default=None, n_args="+")
+    parser.add_argument("--column_names", default=None, nargs="+")
     o = parser.parse_args()
     
     if o.column_names==None:
         t = pd.read_csv(o.fn_input, header=0, sep="\t", compression=o.compression)
     else:
         t = pd.read_csv(o.fn_input, 
-                        header=0, 
                         sep="\t", 
                         compression=o.compression, 
                         header=None,
